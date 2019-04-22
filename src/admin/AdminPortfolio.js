1d747f5ad25125c48850a93a98a25894fef0bc80
marcuspereiragithub <vinnycoast@gmail.com>
2019-04-22 17:01:32 -0300

    Importing storage from firebase-config

--- src/admin/AdminPortfolio.js
+++ src/admin/AdminPortfolio.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import config, { storage } from './../firebase-config';
 
 class AdminPortfolio extends Component {
   constructor(props) {
@@ -7,7 +8,24 @@ class AdminPortfolio extends Component {
 
   render() {
     return (
-      <h2>Portfolio - Area administrativa</h2>
+      <div>
+        <h2>Portfolio - Area administrativa</h2>
+        <form>
+          <div className="form-group">
+            <label for="title">Title</label>
+            <input type="text" className="form-control" id="title" placeholder="Title" />
+          </div>
+          <div className="form-group">
+            <label for="description">Description</label>
+            <textarea class="form-control" id="description" rows="3"></textarea>
+          </div>
+          <div class="form-group">
+            <label for="image">Image File</label>
+            <input type="file" class="form-control-file" id="image" />
+          </div>
+          <button type="submit" className="btn btn-primary">Save</button>
+        </form>
+      </div>
     )
   }
 }
