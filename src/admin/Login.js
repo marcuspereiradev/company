416c732c5e89beb18b800315e6d6d18abd677293
marcuspereiragithub <vinnycoast@gmail.com>
2019-04-23 16:55:56 -0300

    Refactoring form

--- src/admin/Login.js
+++ src/admin/Login.js
@@ -9,18 +9,16 @@ class Login extends Component {
     return (
       <div className="container">
         <h1>Login</h1>
-        <form>
           <div className="form-group">
             <label htmlFor="email">Email address</label>
-            <input type="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" />
+            <input type="email" name="email" ref={(ref) => this.email = ref} className="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" />
             <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input type="password" className="form-control" id="password" placeholder="Password" />
+            <input type="password" name="password" ref={(ref) => this.password = ref} className="form-control" id="password" placeholder="Password" />
           </div>
-          <button type="submit" className="btn btn-primary">Access</button>
-        </form>
+          <button type="button" onclik={this.authenticUser} className="btn btn-primary">Access</button>
       </div>
     )
   }
